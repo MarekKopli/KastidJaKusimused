a50b6669a4a3b31a6aa53e2584e6546e1b189799
marek <marek.kopli@tptlive.ee>
2022-11-09 13:13:28 +0200

    too

---
+++ questions.js
@@ -0,0 +1,17 @@
+import inquirer from 'inquirer';
+inquirer
+    .prompt([
+{type: 'input', name: 'age',message: 'How old are you?'},
+{type: 'input', name: 'how',message: 'How are you?'},
+{type: 'input', name: 'food',message: 'What is your favorite food?'},
+{type: 'input', name: 'height',message: 'How tall are you?'},        
+])
+
+
+.then((answer)=> {
+    console.log(' I am ' + answer.age + ' years old');
+    console.log(' I am ' + answer.how + '');
+    console.log(' My favorite food is ' + answer.food + '');
+    console.log(' I am ' + answer.height + ' centimeters');
+})
+
---
+++ run.js
@@ -0,0 +1,61 @@
+function write(string) {
+    process.stdout.write(string);
+
+}
+import chalk from 'chalk';
+import boxen from 'boxen';
+
+write(chalk.white.bgBlue('CHALK'));
+
+for(let i=0; i<16; i++){
+    for(let j=0; j<16; j++){
+        let color= (i*j).toString()
+        write(chalk.ansi256(color)(color.padEnd(4,' ')));
+    }
+    write('\n');
+}
+
+write(boxen(chalk.blue.bgGreen('Hello chalk'), {
+
+    
+    
+    
+    padding: 2,
+    margin: 1
+}));
+
+write(boxen(chalk.blue.bgGreenBright('Hello chalk'), {
+
+    padding: 1,
+    margin: 3,
+    borderStyle: 'classic'
+
+}));
+
+write(boxen(chalk.blue.bgRedBright('Hello chalk'), {
+    padding: 1,
+    margin: 1,
+    title: 'põnev', 
+    titleAlignment: 'right'
+}));
+
+write(boxen(chalk.blue.bgBlackBright('Hello chalk'), {
+   
+    padding: 1,
+    margin: 5,
+    borderStyle: 'double'
+  
+}));
+
+write(boxen(chalk.blue.bgBlackBright('Hello chalk'), {
+    padding: 1,
+    margin: 8,
+    borderStyle: 'doubleSingle'
+}));
+
+write(boxen(chalk.blue.bgGreen('Hello chalk'), {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'bold',
+    
+}));
