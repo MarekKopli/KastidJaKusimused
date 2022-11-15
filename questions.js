02d14e3d3f4b53b8597141673e48746bfed8206a
Marek Kopli <90028455+MarekKopli@users.noreply.github.com>
2022-11-15 11:39:38 +0200

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,17 +1,25 @@
 import inquirer from 'inquirer';
 inquirer
     .prompt([
-{type: 'input', name: 'age',message: 'How old are you?'},
-{type: 'input', name: 'how',message: 'How are you?'},
-{type: 'input', name: 'food',message: 'What is your favorite food?'},
-{type: 'input', name: 'height',message: 'How tall are you?'},        
+{type: 'input', name: 'input',message: 'How old are you?'},
+{type: 'number', name: 'number',message: '2+8'},
+{type: 'confirm', name: 'confirm',message: 'Do you like pizza?'},
+{type: 'list', name: 'list',message: 'what is your favorite season?', choices: ['winter', 'spring', 'summer', 'autumn']},
+{type: 'checkbox', name: 'checkbox',message: 'What animals do You like?', choices:['cats','lions','elephants','dogs']}, 
+{type:'password', name:'password', message:'What is your password?'},
+{type:'editor', name:'editor', message:'Enter a description'},
+{type:'expand', name:'expand', message:'kits(k) või lammas(l)', choices:[{key:'k',value:'kits'}, {key:'l',value:'lammas',},],},
+
 ])
 
 
 .then((answer)=> {
-    console.log(' I am ' + answer.age + ' years old');
-    console.log(' I am ' + answer.how + '');
-    console.log(' My favorite food is ' + answer.food + '');
-    console.log(' I am ' + answer.height + ' centimeters tall ');
+    console.log(' I am ' + answer.input + ' years old');
+    console.log(' 2+8 is ' + answer.number );
+    console.log(' My favorite food is pizza - ' + answer.confirm );
+    console.log(' My favorite season is ' + answer.list );
+    console.log(' You like ' + answer.checkbox );
+    console.log(' Your password is ' + answer.password);
+    console.log( 'you wrote '+  answer.editor);
+    console.log('kits või lammas? ' + answer.expand);
 })
-
